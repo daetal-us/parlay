549e43e73c977dfe0361265ffcb51ba269d5f2af
Jon Adams <jon@kuokoa.studio>
2017-10-31 10:51:59 -1000

    Leverage mime lib to accept any file; go fmt and updated docs

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
+<img src="http://daetal.us/static/media/dataurl.png" align="right" width="35%">
+
 # Parley
 _A command line utility to generate data URLs for images_
 
-This golang powered command line utility generates a data URL, as specified by [RFC 2397](//tools.ietf.org/html/rfc2397), for a given image.
+This golang powered command line utility generates a data URL, as specified by [RFC 2397](//tools.ietf.org/html/rfc2397), for a given file.
 
 ## Installation
 
@@ -11,6 +13,8 @@ go get github.com/daetal-us/parley
 
 ## Usage
 
+Convert an image to a data url and pipe to clipboard:
+
 ```bash
-parlay path/to/my/image.png
+parlay path/to/my/photo.png | pbcopy
 ```
--- parlay.go
+++ parlay.go
@@ -1,28 +1,34 @@
 package main
 
 import (
-  "io/ioutil"
-  "fmt"
-  "encoding/base64"
-  "os"
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"mime"
+	"os"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
-  if len(os.Args) < 2 {
-    fmt.Println("No filepath specified.")
-    os.Exit(1)
-  }
-  path := os.Args[1]
-  if path == "" {
-    fmt.Println("No filepath specified.")
-    os.Exit(1)
-  }
-  img, err := ioutil.ReadFile(os.Args[1])
-  if err == nil {
-    encodedData := base64.StdEncoding.EncodeToString(img)
-    fmt.Println(fmt.Sprintf(`data:image/png;base64,%s`, encodedData))
-  } else {
-    fmt.Println(err)
-    os.Exit(1)
-  }
+	if len(os.Args) < 2 {
+		fmt.Println("No filepath specified.")
+		os.Exit(1)
+	}
+	path := os.Args[1]
+	if path == "" {
+		fmt.Println("No filepath specified.")
+		os.Exit(1)
+	}
+	img, err := ioutil.ReadFile(os.Args[1])
+	if err == nil {
+		mimeType := strings.Replace(
+			mime.TypeByExtension(filepath.Ext(os.Args[1])), " ", "", -1,
+		)
+		encodedData := base64.StdEncoding.EncodeToString(img)
+		fmt.Println(fmt.Sprintf(`data:%s;base64,%s`, mimeType, encodedData))
+	} else {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }
